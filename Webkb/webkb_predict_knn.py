4c1230c526d1bb4df4a48128bbd33d41666f3d20
while519 <wtyj891@gmail.com>
2017-05-03 10:53:11 +0100

    knn control version is developed, knn=0 can also be valid to concord the const margin

--- Webkb/webkb_predict_knn.py
+++ Webkb/webkb_predict_knn.py
@@ -6,9 +6,6 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from model import *
 from scipy.io.matlab import loadmat
 from scipy.io.matlab import savemat
-import numpy as np
-import theano
-import theano.tensor as T
 import logging
 import math
 import time
@@ -20,12 +17,14 @@ applyfn = 'softmax'
 
 # adjustable parameters
 outdim = 20
-marge_ratio = 2.
+knn = 0
+marge_ratio = 10.
 
 FORMAT = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-_log = logging.getLogger(dataname + ' experiment')
+_log = logging.getLogger(dataname +' experiment')
 _log.setLevel(logging.DEBUG)
-ch_file = logging.FileHandler(filename='lin'+ applyfn + str(outdim) + '_margeratio' + str(marge_ratio) +'.log', mode='w')
+ch_file = logging.FileHandler(filename= 'predict_' + applyfn + str(outdim) + '_knn' +
+                                        str(knn) + '_margeratio' + str(marge_ratio) +  '.log', mode='w')
 ch_file.setLevel(logging.DEBUG)
 ch_file.setFormatter(FORMAT)
 ch = logging.StreamHandler()
@@ -35,27 +34,32 @@ _log.addHandler(ch)
 _log.addHandler(ch_file)
 
 
-
-
-
 # ----------------------------------------------------------------------------
 def SGDexp(state):
     _log.info(state)
     np.random.seed(state.seed)
 
-    # output the initial evaluation based on content only
+    # split the data into training/testing set
+    state.ntrain = math.floor(.9 * state.nlinks)
+    state.ntest = state.nlinks - state.ntrain
+    indices = np.random.permutation(state.nlinks)
+    state.trIdxl = state.Idxl[indices[: state.ntrain]]
+    state.trIdxr = state.Idxr[indices[: state.ntrain]]
+
+    state.teIdxl = state.Idxl[indices[state.ntrain :]]
+    state.teIdxr = state.Idxr[indices[state.ntrain :]]
+
     state.train = np.mean(RankScoreIdx(simi_X, state.trIdxl, state.trIdxr))
     _log.debug('Content Only: Training set Mean Rank: %s ' % (state.train,))
-    state.test = np.mean(RankScoreIdx(simi_wholeX, state.teIdxl, state.teIdxr))
+    state.test = np.mean(RankScoreIdx(simi_X, state.teIdxl, state.teIdxr))
     _log.debug('Content Only: Testing set Mean Rank: %s ' % (state.test,))
 
-
     # initialize
-    mapping = Mappings(np.random, state.nfeatures, state.outdim)  # K x M
+    mapping = Mappings(np.random, state.nsamples, state.outdim)  # K x M
 
     # Function compilation
     apply_fn = eval(state.applyfn)
-    trainfunc = TrainFnMember(apply_fn, X, mapping, state.marge)
+    trainfunc = TrainFn5Member(apply_fn, mapping, P)
 
     out = []
     outb = []
@@ -89,7 +93,7 @@ def SGDexp(state):
 
             if np.mean(out) <= state.bestout:
                 state.bestout = np.mean(out)
-                state.lrmapping *= 1.01
+                state.lrmapping *= 1.1
             else:
                 state.lrmapping *= .01
 
@@ -99,18 +103,17 @@ def SGDexp(state):
             _log.debug('Learning rate: %s LeaveOneOut: %s' % (state.lrmapping, np.mean(outc)))
 
             timeref = time.time()
-            Pr = apply_fn(X.dot(mapping.E.T)).eval()
+            Pr = apply_fn(mapping.E.T).eval()
             state.train = np.mean(RankScoreIdx(Pr, state.trIdxl, state.trIdxr))
             _log.debug('Training set Mean Rank: %s  Score: %s' % (state.train, np.mean(Pr[state.trIdxr, state.trIdxl])))
-            Prall = apply_fn(wholeX.dot(mapping.E.T)).eval()
-            state.test = np.mean(RankScoreIdx(Prall, state.teIdxl, state.teIdxr))
+            state.test = np.mean(RankScoreIdx(Pr, state.teIdxl, state.teIdxr))
             _log.debug('Testing set Mean Rank: %s ' % (state.test, ))
             state.cepoch = epoch_count
             f = open(state.savepath + '/' + 'model' + '.pkl', 'wb')  # + str(state.cepoch)
             pickle.dump(mapping, f, -1)
             f.close()
-            savemat('linpred_dim' + str(state.outdim) + '_method' + state.applyfn +
-                    '_marge' + str(state.marge) + '.mat', {'A': mapping.E.eval()})
+            savemat('pred_dim' + str(state.outdim) + '_method' + state.applyfn +
+                    '_marge' + str(state.marge) + '.mat', {'mappedX': mapping.E.eval()})
             _log.debug('The saving took %s seconds' % (time.time() - timeref))
             timeref = time.time()
 
@@ -120,7 +123,6 @@ def SGDexp(state):
         state.bestout = np.inf
         if state.lrmapping < state.baselr:      # if the learning rate is not growing
             state.baselr *= 0.1
-
         state.lrmapping = state.baselr
         f = open(state.savepath + '/' + 'state.pkl', 'wb')
         pickle.dump(state, f, -1)
@@ -140,42 +142,39 @@ if __name__ == '__main__':
     state.savepath = '../pickled_data'
 
     # load the matlab data file
-    mat = loadmat(datapath + 'processed_' + dataname +'.mat')
-    trY = np.array(mat['trY'], np.float32)
-    trIdx1 = np.array(mat['trRowIdx'], np.int32)
-    trIdx2 = np.array(mat['trColumnIdx'], np.int32)
-    teI = np.array(mat['teI'], np.float32)
-    state.teIdxl = np.asarray(teI[:,0].flatten() -1, dtype='int32')      # numpy indexes start from 0
-    state.teIdxr = np.asarray(teI[:,1].flatten() -1, dtype='int32')
-    wholeX = np.array(mat['Y'], dtype=theano.config.floatX)
-
+    mat = loadmat(datapath + dataname + '.mat')
+    X = np.array(mat['X'], np.float32)
+    I = np.array(mat['I'], np.float32)
+    state.Idxl = np.asarray(I[:, 0].flatten() - 1, dtype='int32')  # numpy indexes start from 0
+    state.Idxr = np.asarray(I[:, 1].flatten() - 1, dtype='int32')
 
     state.seed = 213
     state.totepochs = 1200
-    state.lrmapping = 100.
+    state.lrmapping = 1000.
     state.baselr = state.lrmapping
     state.regterm = .0
-    X = np.asarray(trY, dtype=theano.config.floatX)  # content matrix
-    state.trIdxl = np.asarray(trIdx1.flatten() - 1, dtype='int32')  # matlab to numpy indexes conversion
-    state.trIdxr = np.asarray(trIdx2.flatten() - 1, dtype='int32')
-
-    state.nsamples, state.nfeatures = np.shape(trY)
-    state.ntrain = np.shape(trIdx1)[0]
+    state.nsamples, state.nfeatures = np.shape(X)
+    state.nlinks = np.shape(state.Idxl)[0]
     state.outdim = outdim
     state.applyfn = applyfn
     state.marge = marge_ratio / state.nsamples
     state.nbatches = 1  # mini-batch SGD is not helping here
     state.neval = 10
-    state.lrparam = 1000.
+    state.initial_dim = 300
+
 
     # cosine similarity measure
     simi_X = consine_simi(X)
-    np.fill_diagonal(simi_X, .0)
+    np.fill_diagonal(simi_X, 0)
 
-    simi_wholeX = consine_simi(wholeX)
-    np.fill_diagonal(simi_wholeX, .0)
+    # construct the marginal matrix
+    #np.sort(simi_X, axis=1)
+    dist_X = np.ones(shape=(state.nsamples, state.nsamples))
+    knnIdx = np.argsort(-simi_X.copy(), axis=1)[:,:knn]
+    for rowind in range(state.nsamples):
+        dist_X[rowind, knnIdx[rowind,:]] = .0
 
-    X = T.as_tensor_variable(X)
-    wholeX = T.as_tensor_variable(wholeX)
+    P = T.as_tensor_variable(np.asarray(state.marge * dist_X, dtype=theano.config.floatX))
 
+    # start the experiments
     SGDexp(state)
