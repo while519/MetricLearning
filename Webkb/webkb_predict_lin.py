17effb882d8df46e289b2ba44d925a6a5362d8b1
while519 <wtyj891@gmail.com>
2017-05-03 10:36:11 +0100

    The linear mean rank model is tested

--- Webkb/webkb_predict_lin.py
+++ Webkb/webkb_predict_lin.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from model import *
 from scipy.io.matlab import loadmat
 from scipy.io.matlab import savemat
-import os
 import numpy as np
-import copy
-from collections import OrderedDict
 import theano
 import theano.tensor as T
 import logging
@@ -13,13 +14,18 @@ import math
 import time
 import pickle
 
+# experimental parameters
 dataname = 'webkb'
-applyfn = 'softcauchy'
+applyfn = 'softmax'
+
+# adjustable parameters
 outdim = 20
+marge_ratio = 2.
+
 FORMAT = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 _log = logging.getLogger(dataname + ' experiment')
 _log.setLevel(logging.DEBUG)
-ch_file = logging.FileHandler(filename='lin'+ applyfn + str(outdim) +'.log', mode='w')
+ch_file = logging.FileHandler(filename='lin'+ applyfn + str(outdim) + '_margeratio' + str(marge_ratio) +'.log', mode='w')
 ch_file.setLevel(logging.DEBUG)
 ch_file.setFormatter(FORMAT)
 ch = logging.StreamHandler()
@@ -29,186 +35,20 @@ _log.addHandler(ch)
 _log.addHandler(ch_file)
 
 
-# ----------------------------------------------------------------------------
-class DD(dict):
-    """This class is only used to replace a state variable of Jobman"""
-
-    def __getattr__(self, attr):
-        if attr == '__getstate__':
-            return super(DD, self).__getstate__
-        elif attr == '__setstate__':
-            return super(DD, self).__setstate__
-        elif attr == '__slots__':
-            return super(DD, self).__slots__
-        return self[attr]
-
-    def __setattr__(self, attr, value):
-        assert attr not in ('__getstate__', '__setstate__', '__slots__')
-        self[attr] = value
-
-    def __str__(self):
-        return 'DD%s' % dict(self)
-
-    def __repr__(self):
-        return str(self)
-
-    def __deepcopy__(self, memo):
-        z = DD()
-        for k, kv in self.iteritems():
-            z[k] = copy.deepcopy(kv, memo)
-        return z
 
 
-# Mappings class -----------------------------------------------------------
-class Mappings(object):
-    """Class for the embeddings matrix."""
-
-    def __init__(self, rng, N, D, tag=''):
-        """
-        Constructor.
-
-        :param rng: numpy.random module for number generation.
-        :param N: number of the features.
-        :param D: dimension of the reduced dimensionality.
-        :param tag: name of the mapping matrix.
-        """
-        self.N = N
-        self.D = D
-        wbound = np.sqrt(6. / D)
-        W_values = rng.uniform(low=-wbound, high=wbound, size=(D, N))
-        W_values = W_values / np.sqrt(np.sum(W_values ** 2))  # Columns are normalized
-        W_values = np.asarray(W_values, dtype=theano.config.floatX)
-        self.E = theano.shared(value=W_values, name='E' + tag)
-        # Define a normalization function with respect to the L_2 norm of the
-        # mapping vectors.
-        self.updates = OrderedDict({self.E: self.E / T.sqrt(T.sum(self.E ** 2, axis=0))})
-        self.normalize = theano.function([], [], updates=self.updates)
-
-
-# ----------------------------------------------------------------------------
-def softmax(Y):
-    """
-    turn the distances into probability using the safe softmax function
-    :param X: (M x N) data matrix
-    :param A: (N x K) projection matrix
-    :return: (M x M) probability matrix about the neighbourhood likeness
-    """
-    sumY = T.sum(T.sqr(Y), axis=1)  # column sum, sumY.shape = (M,)
-    Dist = sumY.dimshuffle('x', 0) + sumY.dimshuffle(0, 'x') - 2 * Y.dot(Y.T)
-    max_Dist = T.max(Dist)
-    rebased_Dist = Dist - max_Dist
-    expDist = T.exp(-rebased_Dist)
-    expDist = T.fill_diagonal(expDist, 0)
-    return expDist / T.sum(expDist, axis=1)  # (M, M) / (M,) : column sum is one
-
-
-# ----------------------------------------------------------------------------
-def softcauchy(Y):
-    """
-     turn the distances into probability using the cauchy distribution
-    :param X: X: (M x N) data matrix
-    :param A: (N x K) projection matrix
-    :return: (M x M) probability matrix about the neighbourhood likeness
-    """
-    sumY = T.sum(T.sqr(Y), axis=1)  # column sum, sumY.shape = (M,)
-    dist = sumY.dimshuffle('x', 0) + sumY.dimshuffle(0, 'x') - 2 * Y.dot(Y.T)
-    cauchy_dist = 1 / (dist + 1)
-    cauchy_dist = T.fill_diagonal(cauchy_dist, 0)
-    return cauchy_dist / T.sum(cauchy_dist, axis=1)  # (M, M) / (M,) : column sum is one. Pr(j,i)
-
-
-# ----------------------------------------------------------------------------
-def margincost(pos, neg, marge=.1):
-    out = neg - pos + marge
-    return T.mean(out * (out > 0)), out > 0
-
-
-# ----------------------------------------------------------------------------
-def TrainFnMember(fnPr, X, mapping, marge):             # standard margin-based cost
-    """
-
-    :param fnPr:
-    :param mapping:
-    :param marge:
-    :return:
-    """
-
-    # define the required symbolic input
-    inpl, inpr, inprn = T.ivectors(3)
-    lrmapping = T.scalar('lrmapping')  # learning rate
-
-    list_in = [inpl, inpr, inprn, lrmapping]  # highlighting input argument
-
-    Pr = fnPr(X.dot(mapping.E.T))  # mapping.E (K x N) matrix
-    p = Pr[inpr, inpl]  # (L,)
-    pln = Pr[inprn, inpl]
-    cost, out = margincost(p, pln, marge)
-
-    # assumming no other parameters
-    gradients_mapping = T.grad(cost, mapping.E)
-    newE = mapping.E - lrmapping * gradients_mapping
-    updates = OrderedDict({mapping.E: newE})
-
-    """
-        Theano function inputs.
-        :input lrmapping: learning rate for the mapping matrix.
-        :input inpl: vector representing the indexes of the positive
-                     citations 'left' member, shape=(#examples,).
-        :input inpr: vector representing the indexes of the positive
-                     citations 'right' member, shape=(#examples,).
-        :input inpln: vector representing the indexes of the negative
-                     citations 'left' member, shape=(#examples,).
-
-        Theano function output.
-        :output mean(cost): average cost.
-        :output mean(out): ratio of examples for which the margin is violated,
-                           i.e. for which an update occurs.
-        """
-    return theano.function(list_in, [T.mean(cost), T.mean(out), T.mean(p)],
-                           updates=updates, on_unused_input='ignore')
-
-
-# ----------------------------------------------------------------------------
-def RankScoreIdx(Pr, idxl, idxr):
-    err = []
-    for l, r in zip(idxl, idxr):
-        err += [np.argsort(np.argsort(
-            Pr[:, l])[::-1])[r] + 1]
-    return err
-
-
-# ----------------------------------------------------------------------------
-def pca(X=np.array([]), no_dims=30):
-    """Runs PCA on the MxN array X in order to reduce its dimensionality to no_dims dimensions."""
-
-    print("Preprocessing the data using PCA...")
-    X = X - np.mean(X, 0)  # (M, N) - (N,) using broadcasting
-    (_, v) = np.linalg.eig(np.dot(X.T, X))
-    Y = np.dot(X, v[:, 0:no_dims])
-    return Y
-
-
-# ----------------------------------------------------------------------------
-def consine_simi(X=np.array([])):
-    """
-        Return the cosine similarity matrix for input matrix X
-    :param X: (M x N) sample matrix
-    :return: P: (M x M) consine similarity measure based on the N features
-    """
-    inner_product = np.dot(X, X.T)              # (M, M)
-    square_magnitude = np.diag(inner_product)    # (M,)
-    inv_square_magnitude = 1 / square_magnitude
-
-    inv_square_magnitude[np.isinf(inv_square_magnitude)] = 0
-
-    inv_magnitude = np.sqrt(inv_square_magnitude)
-    cosine = inner_product * inv_magnitude
-    return cosine.T * inv_magnitude
 
 # ----------------------------------------------------------------------------
 def SGDexp(state):
     _log.info(state)
     np.random.seed(state.seed)
+
+    # output the initial evaluation based on content only
+    state.train = np.mean(RankScoreIdx(simi_X, state.trIdxl, state.trIdxr))
+    _log.debug('Content Only: Training set Mean Rank: %s ' % (state.train,))
+    state.test = np.mean(RankScoreIdx(simi_wholeX, state.teIdxl, state.teIdxr))
+    _log.debug('Content Only: Testing set Mean Rank: %s ' % (state.test,))
+
 
     # initialize
     mapping = Mappings(np.random, state.nfeatures, state.outdim)  # K x M
@@ -249,7 +89,7 @@ def SGDexp(state):
 
             if np.mean(out) <= state.bestout:
                 state.bestout = np.mean(out)
-                state.lrmapping *= 1.1
+                state.lrmapping *= 1.01
             else:
                 state.lrmapping *= .01
 
@@ -278,7 +118,10 @@ def SGDexp(state):
         outc = []
         out = []
         state.bestout = np.inf
-        state.lrmapping = 100.
+        if state.lrmapping < state.baselr:      # if the learning rate is not growing
+            state.baselr *= 0.1
+
+        state.lrmapping = state.baselr
         f = open(state.savepath + '/' + 'state.pkl', 'wb')
         pickle.dump(state, f, -1)
         f.close()
@@ -304,16 +147,15 @@ if __name__ == '__main__':
     teI = np.array(mat['teI'], np.float32)
     state.teIdxl = np.asarray(teI[:,0].flatten() -1, dtype='int32')      # numpy indexes start from 0
     state.teIdxr = np.asarray(teI[:,1].flatten() -1, dtype='int32')
-    wholeX = T.as_tensor_variable(np.array(mat['Y'], dtype=theano.config.floatX))
+    wholeX = np.array(mat['Y'], dtype=theano.config.floatX)
 
 
     state.seed = 213
     state.totepochs = 1200
-    state.lrmapping = 1000.
+    state.lrmapping = 100.
+    state.baselr = state.lrmapping
     state.regterm = .0
-    X = T.as_tensor_variable(np.asarray(trY, dtype=theano.config.floatX))  # content matrix
-    # Indpairs = T.cast(theano.shared(np.asarray(np.concatenate([trIdx1, trIdx2], axis=1),
-    #                                    dtype=theano.config.floatX)), 'int32')      # pairs for citation linkages
+    X = np.asarray(trY, dtype=theano.config.floatX)  # content matrix
     state.trIdxl = np.asarray(trIdx1.flatten() - 1, dtype='int32')  # matlab to numpy indexes conversion
     state.trIdxr = np.asarray(trIdx2.flatten() - 1, dtype='int32')
 
@@ -321,9 +163,19 @@ if __name__ == '__main__':
     state.ntrain = np.shape(trIdx1)[0]
     state.outdim = outdim
     state.applyfn = applyfn
-    state.marge = 0.01
+    state.marge = marge_ratio / state.nsamples
     state.nbatches = 1  # mini-batch SGD is not helping here
     state.neval = 10
     state.lrparam = 1000.
+
+    # cosine similarity measure
+    simi_X = consine_simi(X)
+    np.fill_diagonal(simi_X, .0)
+
+    simi_wholeX = consine_simi(wholeX)
+    np.fill_diagonal(simi_wholeX, .0)
+
+    X = T.as_tensor_variable(X)
+    wholeX = T.as_tensor_variable(wholeX)
 
     SGDexp(state)
